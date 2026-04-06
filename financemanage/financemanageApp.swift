a50a712b582fafae38f15205dac92caa601029cb
Dev Malik <devmalik2211@gmail.com>
2026-04-06 23:55:40 +0530

    Initial Commit

---
+++ financemanage/financemanageApp.swift
@@ -0,0 +1,17 @@
+//
+//  financemanageApp.swift
+//  financemanage
+//
+//  Created by DEV on 06/04/26.
+//
+
+import SwiftUI
+
+@main
+struct financemanageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
